25de42c784f95310663e0d628f28f43ceb5d5373
Gilles Huron <gilles.huron@qualtera.com>
2014-06-10 10:32:13 +0200

    create model and market package

--- gobit.go
+++ gobit.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gilleshuron/gobit/market"
+	// "github.com/gilleshuron/gobit/model"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
-} 
+	LOG := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	var (
+		market market.Bitstamp
+	)
+	go market.Start()
+	for {
+		select {
+		case trade := <-market.Trades:
+			LOG.Printf("%s %s. Price:%.2f$ Amount:%.2fBTC Id:%d", trade.Market, trade.Move, trade.Price, trade.Amount, trade.Id)
+		case <-market.Books:
+			// LOG.Printf("%s Bids: %v Asks: %v ", book.Market, book.Bids, book.Asks)
+		default:
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
