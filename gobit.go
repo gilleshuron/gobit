07be7010bcfcbad6e6f17b1d1fd4bb4c6238900a
Gilles Huron <gilles.huron@qualtera.com>
2014-06-12 13:38:56 +0200

    web package

--- gobit.go
+++ gobit.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gilleshuron/gobit/market"
+	"github.com/gilleshuron/gobit/web"
+
 	// "github.com/gilleshuron/gobit/model"
 	"log"
 	"os"
@@ -11,15 +14,19 @@ import (
 func main() {
 	LOG := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	var (
-		market market.Bitstamp
-	)
-	go market.Start()
+	m := market.NewBitstamp()
+	w := web.NewWeb()
+	h := web.H()
+	go h.Run()
+	go m.Run()
+	go w.Run()
 	for {
 		select {
-		case trade := <-market.Trades:
+		case trade := <-m.Trades:
 			LOG.Printf("%s %s. Price:%.2f$ Amount:%.2fBTC Id:%d", trade.Market, trade.Move, trade.Price, trade.Amount, trade.Id)
-		case <-market.Books:
+			j, _ := json.Marshal(trade)
+			h.Broadcast <- j
+		case <-m.Books:
 			// LOG.Printf("%s Bids: %v Asks: %v ", book.Market, book.Bids, book.Asks)
 		default:
 			time.Sleep(10 * time.Millisecond)
